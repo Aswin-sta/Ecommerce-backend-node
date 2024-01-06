6ebacd261a839544d8a2831a1e54896e84b4d1cb
Aswin-Sta <aswin.001.official@gmail.com>
2024-01-06 17:43:30 +0530

    feat:added JWT

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -4,6 +4,7 @@ import EcSuppliers from '../../model/ec_suppliers.ts';
 import EcSuppliersModel from '../../types/ec_suppliers.ts';
 import EcCustomers from '../../model/ec_customers.ts';
 import EcCustomersModel from '../../types/ec_customers.ts';
+import jwt from 'jsonwebtoken'
 
 
   
@@ -21,14 +22,24 @@ indexRouter.post("/login",async (req:Request,res:Response)=>{
   if(LoginData.client_type==="supplier"){
             const result:EcSuppliersModel|null = await EcSuppliers.findOne({where: { e_mail: LoginData.e_mail} })
             if (result?.password===LoginData.password){
-                res.send(result);
+                const token = jwt.sign(
+                    { userId: result.registration_id, client_type:LoginData.client_type },
+                    'your-secret-key', // Replace with your secret key
+                    { expiresIn: '24h' } // Token expiration time
+                  );
+                res.status(200).json({"status":"login succesful","token":token,"expiry":"24h"});
             }else{
                 res.status(401).json("Inavlid client credentials")
             }
   }else if(LoginData.client_type==="customer"){
             const result:EcCustomersModel|null = await EcCustomers.findOne({where: { e_mail: LoginData.e_mail} })
             if (result?.password===LoginData.password){
-                res.send(result);
+                const token = jwt.sign(
+                    { userId: result.registration_id, client_type:LoginData.client_type },
+                    'your-secret-key', // Replace with your secret key
+                    { expiresIn: '24h' } // Token expiration time
+                  );
+                res.status(200).json({"status":"login succesful","token":token,"expiry":"24h"});
             }else{
                 res.status(401).json("Inavlid client credentials")
             }
